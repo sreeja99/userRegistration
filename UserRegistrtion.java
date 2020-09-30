1643f4027b9c954838d63d75645c5d83e1105e21
sreeja99 <srijagodishala@gmail.com>
2020-09-30 16:10:29 +0530

    Modified:Checking Whether Last Name Entered By User is Valid Or Not

--- UserRegistrtion.java
+++ UserRegistrtion.java
@@ -10,11 +10,11 @@ public class UserRegistrtion {
 		Scanner sc=new Scanner(System.in);
 		//welcome statement
 		System.out.println("Welcome to User Registration Program");
-		//condition for name
-		String firstName ="^[A-Z]{1}[a-zA-Z]{2,}$";
-		System.out.println("Enter the Name");
+		//condition for last name
+		String lastName ="^[A-Z]{1}[a-zA-Z]{2,}$";
+		System.out.println("Enter the  Last Name");
 		String  input =sc.next();
-		Pattern pattern =Pattern.compile(firstName);
+		Pattern pattern =Pattern.compile(lastName);
 		Matcher matcher = pattern.matcher(input);
 		System.out.println("Input String matches regex-"+matcher.matches());
 		sc.close();
