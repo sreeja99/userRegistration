5342b01c0a36ae0b9b31ec082fb33c1f453a389d
sreeja99 <srijagodishala@gmail.com>
2020-09-30 16:05:40 +0530

    Modified:Checking Whether the User Has Entered Valid First Name

--- UserRegistrtion.java
+++ UserRegistrtion.java
@@ -1,6 +1,8 @@
 package bridgelabz;
 //importing packages
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserRegistrtion {
 
@@ -8,6 +10,14 @@ public class UserRegistrtion {
 		Scanner sc=new Scanner(System.in);
 		//welcome statement
 		System.out.println("Welcome to User Registration Program");
+		//condition for name
+		String firstName ="^[A-Z]{1}[a-zA-Z]{2,}$";
+		System.out.println("Enter the Name");
+		String  input =sc.next();
+		Pattern pattern =Pattern.compile(firstName);
+		Matcher matcher = pattern.matcher(input);
+		System.out.println("Input String matches regex-"+matcher.matches());
+		sc.close();
 		
 
 	}
