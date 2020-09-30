bb843c032ede48dd8b8cc24323cf138c3dd4c748
sreeja99 <srijagodishala@gmail.com>
2020-09-30 15:53:57 +0530

    Added :Welcome Statement Functionality added

---
+++ UserRegistrtion.java
@@ -0,0 +1,15 @@
+package bridgelabz;
+//importing packages
+import java.util.*;
+
+public class UserRegistrtion {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//welcome statement
+		System.out.println("Welcome to User Registration Program");
+		
+
+	}
+
+}
